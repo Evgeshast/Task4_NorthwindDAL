12cfeaf9d029fd28ce729fb837da594b1b1268b2
Yauheniya Strelkova <Yauheniya_Strelkova@epam.com>
2020-03-19 18:12:54 +0300

    workaround for connection string and test project dependencies fix

--- NorthwindDAL/Database.cs
+++ NorthwindDAL/Database.cs
@@ -5,7 +5,8 @@ namespace NorthwindDAL
 {
     public static class Database
     {
-        public static string ConnectionString => ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public static string ConnectionString =>
+            "Data Source=localhost;Initial Catalog = Northwind; Integrated Security=True";//ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
         public static void OpenConnection()
         {
